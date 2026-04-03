e94158a39a8c9d5540da06217b5fef3fbf1e598d
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-04-03 17:49:58 +0200

    mileston 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,32 @@
-console.log("ciao");
+const distanza = document.querySelector('#input-distanza');
+const btn = document.querySelector('#btn-biglietto');
+const eta = document.querySelector('#input-eta');
+
+
+function stampaBiglietto (){
+    const valoreDistanza = distanza.value;
+    const valoreEta = eta.value;
+    const prezzoAlKm = 0.21;
+    let prezzoBiglietto = 0;
+    const scontoMinorenni = 0.8;
+    const scontoAnziani = 0.6;
+    const prezzoPerDistanza = prezzoAlKm * Number(valoreDistanza);
+
+    if (Number(valoreEta) < 18 && Number(valoreEta) > 10) {
+        prezzoBiglietto = (prezzoPerDistanza * scontoMinorenni).toFixed(2);
+    } else if (Number(valoreEta) > 64) {
+        prezzoBiglietto = (prezzoPerDistanza * scontoAnziani).toFixed(2);
+    } else if (Number(valoreEta) > 17 && Number(valoreEta < 65)){
+        prezzoBiglietto = prezzoPerDistanza.toFixed(2);
+    }
+
+    console.log(prezzoBiglietto);
+    
+    
+    
+}
+
+
+btn.addEventListener (
+    'click', stampaBiglietto
+)
